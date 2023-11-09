741f57cd2a2d0496ec41d15dee57a34ce6d8a9ed
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-11-09 10:20:02 -0500

    second commit

--- src/4.ts
+++ src/4.ts
@@ -1,9 +1,5 @@
 class Key {
-  private signature: number;
-
-  constructor() {
-    this.signature = Math.random();
-  }
+  private signature: number = Math.random();
 
   getSignature(): number {
     return this.signature;
@@ -11,10 +7,7 @@ class Key {
 }
 
 class Person {
-  private key: Key;
-
-  constructor(key: Key) {
-    this.key = key;
+  constructor(private key: Key) {
   }
 
   getKey(): Key {
@@ -24,11 +17,9 @@ class Person {
 
 abstract class House {
   protected door: boolean = false;
-  protected key: Key;
   protected tenants: Person[] = [];
 
-  constructor(key: Key) {
-    this.key = key;
+  constructor(protected key: Key) {
   }
 
   abstract openDoor(key: Key): void;
@@ -44,10 +35,7 @@ abstract class House {
 }
 
 class MyHouse extends House {
-  constructor(key: Key) {
-    super(key);
-  }
-
+  
   openDoor(key: Key): void {
     if (this.key.getSignature() === key.getSignature()) {
       this.door = true;
